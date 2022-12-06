2f2244a221089cb2ae4dba5f2ffd48c16a30bd6c
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-06 21:05:36 +0100

    adds solution for Jewels_and_Stones and installs typescript

---
+++ easy/Jewels_and_Stones/index.ts
@@ -0,0 +1,34 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+function numJewelsInStones(jewels: string, stones: string): number {
+    /* 1
+    let result = 0;
+
+    for(let c of stones) {
+        if(jewels.indexOf(c) !== -1) {
+            result++;
+        }
+    }
+    */
+
+    // 3
+    let result = 0;
+    const set = new Set(jewels);
+    
+    for(let c of stones) {
+        if(set.has(c)) {
+            result++;
+        }
+    }
+    return result;
+    
+    /* 2
+    return jewels.split("").reduce( (acc, value) => acc + stones.split(value).length-1 , 0)
+    */
+}
+
+console.log(numJewelsInStones("aA", "aAAbbbb"), 3);
+console.log(numJewelsInStones("z", "ZZ"), 0);
