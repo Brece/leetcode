0242c92ffa1653d3c872211b5f1c5edeb0945719
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-13 21:08:11 +0100

    adds solution for Contains_Duplicate

---
+++ easy/Contains_Duplicate/index.js
@@ -0,0 +1,18 @@
+"use strict";
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function containsDuplicate(nums) {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (map[nums[i]]) {
+            return true;
+        }
+        map[nums[i]] = true;
+    }
+    return false;
+}
+console.log(containsDuplicate([1, 2, 3, 1]), true);
+console.log(containsDuplicate([1, 2, 3, 4]), false);
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]), true);
---
+++ easy/Contains_Duplicate/index.ts
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function containsDuplicate(nums: number[]): boolean {
+    let map: { [key: number]: boolean } = {};
+
+    for(let i = 0; i < nums.length; i++) {
+        if(map[nums[i]]) {
+            return true;
+        }
+        map[nums[i]] = true;
+    }
+    return false;
+}
+
+console.log(containsDuplicate([1,2,3,1]), true);
+console.log(containsDuplicate([1,2,3,4]), false);
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]), true);
