22e48555f1a9ffaa7dd8a75758f2d9d0e83e7116
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-22 22:13:18 +0100

    adds solution for Fibonacci_Number

---
+++ easy/Fibonacci_Number/index.js
@@ -0,0 +1,33 @@
+"use strict";
+// normal loop approach
+function fibLoop(n) {
+    if (n <= 1) {
+        return n;
+    }
+    let list = [0, 1];
+    for (let i = 1; i < n; i++) {
+        list.push(list[list.length - 1] + list[list.length - 2]);
+    }
+    return list[list.length - 1];
+}
+// recursion approach
+function fibRec(n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibRec(n - 1) + fibRec(n - 2);
+}
+console.log(fibLoop(0), 0);
+console.log(fibLoop(1), 1);
+console.log(fibLoop(2), 1);
+console.log(fibLoop(3), 2);
+console.log(fibLoop(4), 3);
+console.log(fibLoop(5), 5);
+console.log(fibLoop(6), 8);
+console.log(fibRec(0), 0);
+console.log(fibRec(1), 1);
+console.log(fibRec(2), 1);
+console.log(fibRec(3), 2);
+console.log(fibRec(4), 3);
+console.log(fibRec(5), 5);
+console.log(fibRec(6), 8);
